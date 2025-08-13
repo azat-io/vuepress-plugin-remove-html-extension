e6918d40949ecba5773d7960de05509269125c94
Azat S. <to@azat.io>
2025-08-13 22:52:32 +0300

    chore: update dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,24 @@
 import type { PluginFunction } from '@vuepress/core'
 
-export let removeHtmlExtensionPlugin = (): PluginFunction => () => ({
-  extendsPage: page => {
-    let { frontmatter = {}, path } = page
+export function removeHtmlExtensionPlugin(): PluginFunction {
+  return function () {
+    return {
+      extendsPage: page => {
+        let { frontmatter = {}, path } = page
 
-    if (frontmatter.permalink) {
-      return
-    }
+        if (frontmatter.permalink) {
+          return
+        }
 
-    if (path === '/404.html') {
-      page.path = '/404.html'
-    } else if (path.endsWith('.html')) {
-      page.path = path.slice(0, -5)
-    } else if (path.endsWith('/')) {
-      page.path = `${path.slice(0, -1)}/`
+        if (path === '/404.html') {
+          page.path = '/404.html'
+        } else if (path.endsWith('.html')) {
+          page.path = path.slice(0, -5)
+        } else if (path.endsWith('/')) {
+          page.path = `${path.slice(0, -1)}/`
+        }
+      },
+      name: 'vuepress-plugin-remove-html-extension',
     }
-  },
-  name: 'vuepress-plugin-remove-html-extension',
-})
+  }
+}
